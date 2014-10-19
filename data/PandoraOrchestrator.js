6ad4fc03262fdf24ac162bc37a5ffc3dca8b4f7c
Carlin <locketine@gmail.com>
2014-10-18 18:13:38 -0700

    No longer translating event.key to custom string. Just passing it to the workers if it's a media event.

--- data/PandoraOrchestrator.js
+++ data/PandoraOrchestrator.js
@@ -2,13 +2,28 @@ var getElementByXpath = function (path) {
  return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 };
 
-self.port.on("TogglePlay", function(){
-	var playButton = getElementByXpath("//div[@class='playButton']");
+var playButtonXpath = "//div[@class='playButton']";
+var pauseButtonXpath = "//div[@class='skipButton']";
+
+self.port.on("MediaPlayPause", function(){
+	var playButton = getElementByXpath(playButtonXpath);
 	if (playButton == null) return;
 	if (playButton.getAttribute("style").indexOf("block") > -1) playButton.click();
 	else {
 		var pauseButton = getElementByXpath("//div[@class='pauseButton']");
 		pauseButton.click();
 	}
-	self.port.emit("PlayToggled");
+	self.port.emit("MediaPlayPause");
+});
+
+self.port.on("MediaNextTrack", function(){
+	var skipButton = getElementByXpath(pauseButtonXpath);
+	if (skipButton == null) return;
+	skipButton.click();
+	self.port.emit("MediaNextTrack");
+});
+
+self.port.on("MediaStop", function(){
+	self.window.close();
+	self.port.emit("MediaNextTrack");
 });
