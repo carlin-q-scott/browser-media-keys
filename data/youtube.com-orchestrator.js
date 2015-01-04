0d53dcacb23d7d1218d50bc8a53919407c35c6b9
Carlin <locketine@gmail.com>
2015-01-03 20:54:55 -0800

    Added support for youtube.com

---
+++ data/youtube.com-orchestrator.js
@@ -0,0 +1,42 @@
+/**
+ * MediaKeys namespace.
+ */
+if (typeof MediaKeys == "undefined") var MediaKeys = {};
+
+MediaKeys.Init = function(undefined)
+{
+	var PlayerStates = {
+		unstarted: -1,
+		ended: 0,
+		playing: 1,
+		paused: 2
+	};
+	
+	self.port.on("MediaPlayPause", function(){
+		var player = unsafeWindow.document.getElementById("movie_player");
+		var status = player.getPlayerState();
+		if (status != PlayerStates.playing)
+		{
+			player.playVideo();
+		}
+		else
+		{
+			player.pauseVideo();
+		}
+		self.port.emit("MediaPlayPause");
+	});
+
+	self.port.on("MediaNextTrack", function(){
+		unsafeWindow.document.getElementById("movie_player").nextVideo()
+	});
+
+	self.port.on("MediaPreviousTrack", function(){
+		unsafeWindow.document.getElementById("movie_player").previousVideo()
+	});
+
+	self.port.on("MediaStop", function(){
+		unsafeWindow.document.getElementById("movie_player").stopVideo()
+	});
+}
+
+MediaKeys.Init();
---
+++ data/youtube.com-view.js
@@ -0,0 +1,9 @@
+/**
+ * MediaKeys namespace.
+ */
+if (typeof MediaKeys == "undefined") var MediaKeys = {};
+
+MediaKeys.playButton = "//div[@id='player-api']";
+MediaKeys.pauseButton = MediaKeys.playButton
+MediaKeys.skipButton = "";
+MediaKeys.previousButton = "";
