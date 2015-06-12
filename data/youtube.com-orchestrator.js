632cbd62679f1753822b3a3d5a99046e6656ca79
Carlin Scott <locketine@gmail.com>
2015-06-11 22:17:24 -0700

    Fixed youtube play next/previous and dealt with unsafeWindow warning by injecting player code into youtube page.

--- data/youtube.com-orchestrator.js
+++ data/youtube.com-orchestrator.js
@@ -2,41 +2,78 @@
  * MediaKeys namespace.
  */
 if (typeof MediaKeys == "undefined") var MediaKeys = {};
+if (typeof MediaKeys.pageDomain == "undefined") MediaKeys.pageDomain = "https://www.youtube.com";
 
 MediaKeys.Init = function(undefined)
 {
-	var PlayerStates = {
-		unstarted: -1,
-		ended: 0,
-		playing: 1,
-		paused: 2
-	};
-	
-	self.port.on("MediaPlayPause", function(){
-		var player = unsafeWindow.document.getElementById("movie_player");
-		var status = player.getPlayerState();
-		if (status != PlayerStates.playing)
-		{
-			player.playVideo();
-		}
-		else
-		{
-			player.pauseVideo();
-		}
-		self.port.emit("MediaPlayPause");
-	});
+    //add page side script
+    var pageScript = document.createElement("script");
+    pageScript.innerHTML = `
+        var pageDomain = "` + MediaKeys.pageDomain + `";
+        var PlayerStates = {
+            unstarted: -1,
+            ended: 0,
+            playing: 1,
+            paused: 2
+        };
+        window.addEventListener("message", function(event) {
+            var playVideo = function(){
+                document.getElementById("movie_player").playVideo();
+                window.postMessage("Play", pageDomain);
+            };
+            var pauseVideo = function(){
+                document.getElementById("movie_player").pauseVideo();
+                window.postMessage("Pause", pageDomain);
+            };
 
-	self.port.on("MediaNextTrack", function(){
-		unsafeWindow.document.getElementById("movie_player").nextVideo()
-	});
+            switch (event.data) {
+                case "MediaPlayPause":
+                    var player = document.getElementById("movie_player");
+                    var status = player.getPlayerState();
+                    if (status != PlayerStates.playing) {
+                        playVideo();
+                    }
+                    else {
+                        pauseVideo();
+                    }
+                    break;
 
-	self.port.on("MediaPreviousTrack", function(){
-		unsafeWindow.document.getElementById("movie_player").previousVideo()
-	});
+                case "MediaPause":
+                    pauseVideo();
+                    break;
 
-	self.port.on("MediaStop", function(){
-		unsafeWindow.document.getElementById("movie_player").stopVideo()
-	});
+                case "MediaPlay":
+                    playVideo();
+                    break;
+
+                case "MediaTrackNext":
+                    document.getElementById("movie_player").nextVideo();
+                    window.postMessage("Next", pageDomain);
+                    break;
+
+                case "MediaTrackPrevious":
+                    document.getElementById("movie_player").previousVideo();
+                    window.postMessage("Previous", pageDomain);
+                    break;
+
+                case "MediaStop":
+                    document.getElementById("movie_player").stopVideo();
+                    window.postMessage("Stop", pageDomain);
+                    break;
+            }
+        });`;
+    document.body.appendChild(pageScript);
+
+    self.port.on("MediaPlayPause", function(){ window.postMessage("MediaPlayPause", MediaKeys.pageDomain) });
+    self.port.on("MediaPlay", function(){ window.postMessage("MediaPlay", MediaKeys.pageDomain) });
+    self.port.on("MediaPause", function(){ window.postMessage("MediaPause", MediaKeys.pageDomain) });
+    self.port.on("MediaTrackNext", function(){ window.postMessage("MediaTrackNext", MediaKeys.pageDomain) });
+    self.port.on("MediaTrackPrevious", function(){ window.postMessage("MediaTrackPrevious", MediaKeys.pageDomain) });
+    self.port.on("MediaStop", function(){ window.postMessage("MediaStop", MediaKeys.pageDomain) });
+
+    window.addEventListener("message", function(event) {
+        self.port.emit(event.data);
+    });
 };
 
 MediaKeys.Init();
---
+++ tests/test-youtube-orchestrator.js
@@ -0,0 +1,73 @@
+var tabs = require("sdk/tabs");
+var { viewFor } = require("sdk/view/core");
+var { data } = require("sdk/self");
+var pageWorker;
+
+function OpenMediaWebsiteMock(pageDomain, done)
+{
+    var MediaKeys = {pageDomain: "*"};
+    tabs.once('ready', function(tab) {
+        pageWorker = tab.attach({
+            contentScriptFile: "./youtube.com-orchestrator.js"
+        });
+        done();
+    });
+
+    tabs[0].url = data.url(pageDomain + ".html");
+}
+
+exports["test play on YouTube.com"] = function(assert, done)
+{
+    OpenMediaWebsiteMock("youtube.com", function() {
+        TestMediaEvent("MediaPlay", "Play", assert, done);
+    });
+};
+
+exports["test pause playing on YouTube.com"] = function(assert, done)
+{
+    OpenMediaWebsiteMock("youtube.com", function() {
+        TestMediaEvent("MediaPause", "Pause", assert, done);
+    });
+};
+
+exports["test play/pause on YouTube.com"] = function(assert, done)
+{
+    OpenMediaWebsiteMock("youtube.com", function() {
+        TestMediaEvent("MediaPlayPause", "Pause", assert, function () {
+            TestMediaEvent("MediaPlayPause", "Play", assert, done);
+        });
+    });
+};
+
+exports["test play next track on YouTube.com"] = function(assert, done)
+{
+    OpenMediaWebsiteMock("youtube.com", function() {
+        TestMediaEvent("MediaTrackNext", "Next", assert, done);
+    });
+};
+
+exports["test play previous track on YouTube.com"] = function(assert, done)
+{
+    OpenMediaWebsiteMock("youtube.com", function() {
+        TestMediaEvent("MediaTrackPrevious", "Previous", assert, done);
+    });
+};
+
+exports["test stop playing on YouTube.com"] = function(assert, done)
+{
+    OpenMediaWebsiteMock("youtube.com", function() {
+        TestMediaEvent("MediaStop", "Stop", assert, done);
+    });
+};
+
+function TestMediaEvent(mediaEvent, mediaEventOutcome, assert, done)
+{
+    pageWorker.port.once(mediaEventOutcome, function() {
+        assert.ok(true, mediaEvent + " successfully triggered outcome: " + mediaEventOutcome);
+        done();
+    });
+
+    pageWorker.port.emit(mediaEvent);
+}
+
+require("sdk/test").run(exports);
