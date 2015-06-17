3f8de99b2e26dc8bfd679fcc81bb2023a6d600c2
Carlin Scott <locketine@gmail.com>
2015-06-16 21:47:57 -0700

    Only executing platform specific tests on targeted platform.

--- tests/test-linuxDBusHotkeys.js
+++ tests/test-linuxDBusHotkeys.js
@@ -1,12 +1,13 @@
-var hotkeyWorker = require("../lib/linuxDBusHotkeys");
+if (require("sdk/system").platform == "linux") {
+	var hotkeyWorker = require("../lib/linuxDBusHotkeys");
 
-exports["test register/de-register hotkeys"] = function(assert)
-{
-	hotkeyWorker.postMessage("attach");
-	assert.pass("successfully registered hotkeys");
-    //Todo: something to test that the hotkeys are registered
-	hotkeyWorker.postMessage("detach");
-	assert.pass("successfully de-registered hotkeys");
-};
+	exports["test register/de-register hotkeys"] = function (assert) {
+		hotkeyWorker.postMessage("attach");
+		assert.pass("successfully registered hotkeys");
+		//Todo: something to test that the hotkeys are registered
+		hotkeyWorker.postMessage("detach");
+		assert.pass("successfully de-registered hotkeys");
+	};
 
-require("sdk/test").run(exports);
+	require("sdk/test").run(exports);
+}
