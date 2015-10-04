0c7159be2052359a4fe07d24f0a9443fec7a28ff
michael <pfitzner@physik.hu-berlin.de>
2015-10-04 17:08:18 +0200

    Make Jamstash domains configurable

--- data/Jamstash.js
+++ data/Jamstash.js
@@ -6,6 +6,8 @@
  */
 if (typeof MediaKeys == "undefined") var MediaKeys = {};
 
+console.log("jamstash loaded");
+
 MediaKeys.playButton = "//a[contains(@class, 'PlayTrack') and not(contains(@style,'display: none'))]";
 MediaKeys.pauseButton = "//a[contains(@class, 'PauseTrack') and not(contains(@style,'display: none'))]";
 MediaKeys.skipButton = "//a[@id='NextTrack']";
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-var JamstashDomains = ["jamstash.com"];
-var supportedPageDomains = ["pandora.com", "tidalhifi.com", "youtube.com", "bandcamp.com", "play.google.com", "player.spotify.com", "soundcloud.com"].concat(JamstashDomains);
+var preferences = require("sdk/simple-prefs");
+var JamstashDomains = preferences.prefs.JamstashDomains.split(" ");
+var supportedPageDomains_raw = ["pandora.com", "tidalhifi.com", "youtube.com", "bandcamp.com", "play.google.com", "player.spotify.com", "soundcloud.com"].concat(JamstashDomains);
 var hotkeyManager;
+
+var supportedPageDomains = supportedPageDomains_raw
 
 //attach content scripts to appropriate websites
 exports.main = function (options, callbacks) {
@@ -12,3 +15,14 @@ exports.onUnload = function (reason) {
     hotkeyManager.UnregisterHotkeys();
     hotkeyManager = null;
 };
+
+function onPrefChange(prefName) {
+	JamstashDomains = preferences.prefs.JamstashDomains.split(" ");
+	supportedPageDomains = supportedPageDomains_raw.concat(JamstashDomains);
+	hotkeyManager.UnregisterHotkeys();
+    hotkeyManager = null;
+	hotkeyManager = require("./lib/hotkeyManager");
+    hotkeyManager.RegisterContentScripts(supportedPageDomains, JamstashDomains);
+}
+
+preferences.on("JamstashDomains", onPrefChange);
