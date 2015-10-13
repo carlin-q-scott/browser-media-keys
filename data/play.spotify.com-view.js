3324aa52f7a065649601e13bf6eb7b4110441bfc
Tim Bücher <timbuecher@gmail.com>
2015-10-13 15:08:51 +0200

    Fix spotify bindings

---
+++ data/play.spotify.com-view.js
@@ -0,0 +1,10 @@
+/**
+ * MediaKeys namespace.
+ */
+if (typeof MediaKeys == "undefined") var MediaKeys = {};
+
+MediaKeys.basePlayer = "//iframe[@id='app-player']"
+MediaKeys.playButton = "//button[@id='play-pause']";
+MediaKeys.pauseButton = "//button[@id='play-pause']";
+MediaKeys.skipButton = "//button[@id='next']";
+MediaKeys.previousButton = "//button[@id='previous']";
