fc857c87e1f04e7e74caa5919d66fbb6314fd816
mFoxRU <mfoxru@gmail.com>
2015-12-04 08:13:05 +0300

    Added: Limited radio.yandex.ru support
    Only play/pause for now, next track seems to be complicated (Well,
    that's better than nothing!)

---
+++ data/radio.yandex.ru-view.js
@@ -0,0 +1,7 @@
+/**
+ * MediaKeys namespace.
+ */
+if (typeof MediaKeys == "undefined") var MediaKeys = {};
+
+MediaKeys.playButton = "//div[@class='player-controls__play']";
+MediaKeys.pauseButton = "//div[@class='player-controls__play']";
