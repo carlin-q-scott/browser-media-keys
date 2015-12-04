41f9415d2cf976cee751f08a61ee82554d0d59af
mFoxRU <mfoxru@gmail.com>
2015-12-04 09:33:03 +0300

    Added: Skip track for radio.yandex.ru

--- data/radio.yandex.ru-view.js
+++ data/radio.yandex.ru-view.js
@@ -5,3 +5,4 @@ if (typeof MediaKeys == "undefined") var MediaKeys = {};
 
 MediaKeys.playButton = "//div[@class='player-controls__play']";
 MediaKeys.pauseButton = "//div[@class='player-controls__play']";
+MediaKeys.skipButton = "//div[@class='slider__item slider__item_track'][4]//button";
