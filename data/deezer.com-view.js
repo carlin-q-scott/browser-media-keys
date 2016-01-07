1b7fc988b69da6ff7d3f00a3a474237a99fb6262
Vasili Gulevich <V.V.Gulevich@mail.ru>
2016-01-07 16:00:22 +0600

    Deezer.com support

---
+++ data/deezer.com-view.js
@@ -0,0 +1,9 @@
+/**
+ * MediaKeys namespace.
+ */
+if (typeof MediaKeys == "undefined") var MediaKeys = {};
+
+MediaKeys.playButton = "//button[@class='control control-play']";
+MediaKeys.pauseButton = "//button[@class='control control-pause']";
+MediaKeys.skipButton = "//button[@class='control control-next']";
+MediaKeys.previousButton = "//button[@class='control control-prev']";
