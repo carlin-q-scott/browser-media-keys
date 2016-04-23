939cd5853d0e16fe8304aa8b7a59931fe406113a
kosmodrey <kosmodrey@yandex.ru>
2016-04-23 17:09:22 +0300

    Add "new.vk.com"

---
+++ data/new.vk.com-view.js
@@ -0,0 +1,9 @@
+/**
+ * MediaKeys namespace.
+ */
+if (typeof MediaKeys == "undefined") var MediaKeys = {};
+
+MediaKeys.playButton = "//div[(@class='top_audio_player_btn top_audio_player_play')]";
+MediaKeys.pauseButton = "//div[(@class='top_audio_player_btn top_audio_player_play')]";
+MediaKeys.skipButton = "//div[@class='top_audio_player_btn top_audio_player_next']";
+MediaKeys.previousButton = "//div[@class='top_audio_player_btn top_audio_player_prev']";
