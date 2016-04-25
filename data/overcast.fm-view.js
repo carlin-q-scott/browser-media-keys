d79c4bc173dd80264f3df11a0338b52517dc42c6
Koen Kanters <koenkanters94@gmail.com>
2016-04-25 15:51:24 +0200

    Support for overcast.fm

---
+++ data/overcast.fm-view.js
@@ -0,0 +1,9 @@
+/**
+ * MediaKeys namespace.
+ */
+if (typeof MediaKeys == "undefined") var MediaKeys = {};
+
+MediaKeys.playButton = "//a[@id='playpausebutton']";
+MediaKeys.pauseButton = "//a[@id='playpausebutton']";
+MediaKeys.skipButton = "//a[@id='seekforwardbutton']";
+MediaKeys.previousButton = "//a[@id='seekbackbutton']";
