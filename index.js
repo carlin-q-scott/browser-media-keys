ffac13562e7ab6fa1aa31c4e3c40fe7b9bdaa4ee
Carlin <carlin.q.scott@gmail.com>
2016-05-26 07:40:05 -0700

    Removing pinner on unload/pref change

--- index.js
+++ index.js
@@ -4,17 +4,18 @@ var pageWorkerManager = require("./lib/pageWorkerManager");
 
 //attach content scripts to appropriate websites
 exports.main = function (options, callbacks) {
-    pageWorkerManager.RegisterContentScripts();
+    pageWorkerManager.Init();
 };
 
 exports.onUnload = function (reason) {
     hotkeyManager.UnregisterHotkeys();
+    pageWorkerManager.Destroy();
 };
 
 function onPrefChange() { //re-register content scripts
     hotkeyManager.UnregisterHotkeys();
     pageWorkerManager.Destroy();
-    pageWorkerManager.RegisterContentScripts();
+    pageWorkerManager.Init();
 }
 
 preferences.on("", onPrefChange);
