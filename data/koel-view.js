849a2eaa384f413c1253f779a90df3c03136ab78
Carlin Scott <carlin.q.scott@gmail.com>
2016-06-18 14:33:02 -0700

    Removed some classes from the Koel button xpaths

--- data/koel-view.js
+++ data/koel-view.js
@@ -10,5 +10,5 @@ console.log("koel loaded");
 
 MediaKeys.playButton = "//span[contains(@class, 'play control') and not(contains(@style,'display: none'))]";
 MediaKeys.pauseButton = "//span[contains(@class, 'pause control') and not(contains(@style,'display: none'))]";
-MediaKeys.skipButton = "//i[contains(@class, 'next fa fa-step-forward control')]";
-MediaKeys.previousButton = "//i[contains(@class, 'prev fa fa-step-backward control')]";
+MediaKeys.skipButton = "//i[contains(@class, 'next control')]";
+MediaKeys.previousButton = "//i[contains(@class, 'prev control')]";
