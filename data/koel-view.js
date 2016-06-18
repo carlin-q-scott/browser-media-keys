1d0d8a30099ad60379c717bb00bec742af512783
Carlin Scott <carlin.q.scott@gmail.com>
2016-06-18 14:41:23 -0700

    Last time I mess with Koel-view I swear

--- data/koel-view.js
+++ data/koel-view.js
@@ -8,7 +8,7 @@ if (typeof MediaKeys == "undefined") var MediaKeys = {};
 
 console.log("koel loaded");
 
-MediaKeys.playButton = "//span[contains(@class, 'play') and not(contains(@style,'display: none'))]";
-MediaKeys.pauseButton = "//span[contains(@class, 'pause') and not(contains(@style,'display: none'))]";
-MediaKeys.skipButton = "//i[contains(@class, 'next')]";
-MediaKeys.previousButton = "//i[contains(@class, 'prev')]";
+MediaKeys.playButton = "//span[contains(@class, 'play') and contains(@class,'control') and not(contains(@style,'display: none'))]";
+MediaKeys.pauseButton = "//span[contains(@class, 'pause') and contains(@class,'control') and not(contains(@style,'display: none'))]";
+MediaKeys.skipButton = "//i[contains(@class, 'next') and contains(@class,'control')]";
+MediaKeys.previousButton = "//i[contains(@class, 'prev') and contains(@class,'control')]";
