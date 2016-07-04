d24bc213a99f3c2f8a14ca0081b4c299c4b5426a
Allan Bush <allan.bush@disney.com>
2016-07-04 15:10:25 -0700

    Adding support for the cbcmusic.ca website.

---
+++ data/cbcmusic.ca-view.js
@@ -0,0 +1,9 @@
+/**
+ * MediaKeys namespace.
+ */
+if (typeof MediaKeys == "undefined") var MediaKeys = {};
+
+MediaKeys.playButton = "//button[@class='playButton Play']";
+MediaKeys.pauseButton = "//button[@class='playButton Pause']";
+MediaKeys.skipButton = "//a[@class='player-console-ui-nextButton player-console-ui-functional']";
+MediaKeys.previousButton = "//a[@class='player-console-ui-prevButton player-console-ui-functional']";
