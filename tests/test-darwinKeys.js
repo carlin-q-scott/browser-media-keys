2c7fc5075dceeef8867b9386b66c0758588d784e
Carlin <carlin.q.scott@gmail.com>
2016-07-04 15:15:43 -0700

    dealing with some lint

--- tests/test-darwinKeys.js
+++ tests/test-darwinKeys.js
@@ -11,28 +11,52 @@ if (require("sdk/system").platform == "darwin") {
             hotkeyWorker.postMessage("attach");
             setTimeout(resolve, 500);
         })
-		.then(function(){ 
+        .then(function(){ 
+            return new Promise(function (resolve) {
+                assert.pass("successfully registered hotkeys");
+                hotkeyWorker.postMessage("detach");
+                setTimeout(resolve, 500);
+            });
+        })
+        .then(function(){
+            assert.pass("successfully de-registered hotkeys");
+            done();
+        })
+        .catch(function(error){
+            assert.fail(error);
+            done();
+        });
+    };
+    
+    exports["test play/pause hotkey"] = function (assert, done) {
+        let hotkeyWorker = require("../lib/darwinKeys.js");
+
+        new Promise(function(resolve) {
+            hotkeyWorker.postMessage("attach");
+            setTimeout(resolve, 500);
+        })
+        .then(function(){ 
             return new Promise(function (resolve) {
                 assert.pass("successfully registered hotkeys");
         
                 hotkeyWorker.addEventListener(function(event){
                     assert.equal("MediaPlayPause", event.data, "emmit media play/pause event");
                     resolve();
-                })
+                });
                 notifications.notify({ text: "press play/pause button" });
             });
         })
-		.then(function(){ 
+        .then(function(){ 
             return new Promise(function(resolve){
                 hotkeyWorker.postMessage("detach");
                 setTimeout(resolve, 500);
             });
         })
-		.then(function(){
+        .then(function(){
             assert.pass("successfully de-registered hotkeys");
             done();
         })
-		.catch(function(error){
+        .catch(function(error){
             assert.fail(error);
             done();
         });
