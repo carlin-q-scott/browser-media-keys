916fca2698c3985d20323cde1e343fa8c46b7084
sutr90 <sutr90@gmail.com>
2016-08-04 17:47:04 +0200

    Added support for mixcloud.com

---
+++ data/mixcloud.com-view.js
@@ -0,0 +1,7 @@
+/**
+ * MediaKeys namespace.
+ */
+if (typeof MediaKeys == "undefined") var MediaKeys = {};
+
+MediaKeys.playButton = "//div[(@class='player-control')]";
+MediaKeys.pauseButton = "//div[(@class='player-control pause-state')]";
