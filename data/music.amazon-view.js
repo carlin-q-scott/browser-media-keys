e88786a8269596f6a6ec7a0d4a534f5805cbfedd
Carlin <carlin.q.scott@gmail.com>
2016-08-17 21:08:27 -0700

    Fixed #27 by making the pause/play button xpaths more specific.

--- data/music.amazon-view.js
+++ data/music.amazon-view.js
@@ -3,8 +3,8 @@
  */
 if (typeof MediaKeys == "undefined") var MediaKeys = {};
 
-MediaKeys.playButton = "//*[contains(@class, 'playerIconPlay')]";
-MediaKeys.pauseButton = "//*[contains(@class, 'playerIconPause')]";
+MediaKeys.playButton = "//section[contains(@class,'playbackControlsView')]//*[contains(@class, 'playerIconPlay')]";
+MediaKeys.pauseButton = "//section[contains(@class,'playbackControlsView')]//*[contains(@class, 'playerIconPause')]";
 MediaKeys.skipButton = "//*[contains(@class, 'nextButton')]";
 MediaKeys.previousButton = "//*[contains(@class, 'previousButton')]";
 
