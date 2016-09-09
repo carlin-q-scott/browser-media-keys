85ea936813ad3d274612aec5b05c5c0c77ea8d27
JK <jvsk38@gmail.com>
2016-09-09 16:17:20 +0530

    Hype Machine support

---
+++ data/hypem.com-view.js
@@ -0,0 +1,9 @@
+/**
+ * MediaKeys namespace.
+ */
+if (typeof MediaKeys == "undefined") var MediaKeys = {};
+
+MediaKeys.playButton = "//a[@id='playerPlay']";
+MediaKeys.pauseButton = "//a[@id='playerPlay']";
+MediaKeys.skipButton = "//a[@id='playerNext']";
+MediaKeys.previousButton = "//a[@id='playerPrev']";
