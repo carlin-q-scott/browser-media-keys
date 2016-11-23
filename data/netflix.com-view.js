859b90451d97ea0c5b09a98246aa0343cd5905c7
jakusotsu <raymondlee8888@gmail.com>
2016-11-23 01:09:41 -0800

    Update netflix.com-view.js
    Adding stop and skip

--- data/netflix.com-view.js
+++ data/netflix.com-view.js
@@ -6,5 +6,6 @@ if (typeof MediaKeys == "undefined") var MediaKeys = {};
 
 MediaKeys.playButton = "//div[@class='player-control-button player-play-pause play icon-player-play' and not(contains(@style,'none'))]";
 MediaKeys.pauseButton = "//div[@class='player-control-button player-play-pause pause icon-player-play' and not(contains(@style,'none'))]";
-
+MediaKeys.skipButton = "//div[@class='player-control-a11y-button']";
+MediaKeys.stopButton = "//a[@class='player-back-to-browsing no-select container-icon-player-back-to-browse']";
 
