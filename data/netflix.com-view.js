09c1f383ac8212762bbb6e6ef5ba5cab4501b7f9
jakusotsu <raymondlee8888@gmail.com>
2016-11-23 02:35:52 -0800

    Update netflix.com-view.js
    change back to previousbutton

--- data/netflix.com-view.js
+++ data/netflix.com-view.js
@@ -7,4 +7,4 @@ if (typeof MediaKeys == "undefined") var MediaKeys = {};
 MediaKeys.playButton = "//div[@class='player-control-button player-play-pause play icon-player-play']";
 MediaKeys.pauseButton = "//div[@class='player-control-button player-play-pause pause icon-player-pause']";
 MediaKeys.skipButton = "//div[@class='player-control-button player-next-episode container-icon-player-next-episode']";
-MediaKeys.stopButton = "//a[contains(@class,'player-back-to-browsing')]";
+MediaKeys.previousButton = "//a[contains(@class,'player-back-to-browsing')]";
