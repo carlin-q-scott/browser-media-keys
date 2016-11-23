036cbef7fb6ea0a7f23623412c57d880837e39c9
jakusotsu <raymondlee8888@gmail.com>
2016-11-23 02:54:06 -0800

    Create di.fm-view.js

---
+++ data/di.fm-view.js
@@ -0,0 +1,9 @@
+
+/**
+ * MediaKeys namespace.
+ */
+if (typeof MediaKeys == "undefined") var MediaKeys = {};
+
+var baseXpath = "//div[@class='controls']";
+MediaKeys.playButton = baseXpath + "//div";
+MediaKeys.playButton = baseXpath + "//div";
