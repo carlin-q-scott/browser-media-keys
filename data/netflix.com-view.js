28f4d6537003643e622113a33031ae35c60db0e1
jakusotsu <raymondlee8888@gmail.com>
2016-11-23 01:02:13 -0800

    Update netflix.com-view.js
    Add play and pause

--- data/netflix.com-view.js
+++ data/netflix.com-view.js
@@ -1,1 +1,10 @@
 
+/**
+ * MediaKeys namespace.
+ */
+if (typeof MediaKeys == "undefined") var MediaKeys = {};
+
+MediaKeys.playButton = "//div[@class='player-control-button player-play-pause play icon-player-play' and not(contains(@style,'none'))]";
+MediaKeys.pauseButton = "//div[@class='player-control-button player-play-pause pause icon-player-play' and not(contains(@style,'none'))]";
+
+
