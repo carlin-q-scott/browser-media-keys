66d992513f1e0368b79fcd3499b988aabadf7d61
jakusotsu <raymondlee8888@gmail.com>
2016-11-23 00:58:31 -0800

    Update tunein.com-view.js
    Add mediakeys initializations

--- data/tunein.com-view.js
+++ data/tunein.com-view.js
@@ -1,1 +1,8 @@
 
+/**
+ * MediaKeys namespace.
+ */
+if (typeof MediaKeys == "undefined") var MediaKeys = {};
+
+MediaKeys.playButton = "//div[@class='playbutton-cont col' and not(contains(@style,'none'))]";
+MediaKeys.pauseButton = "//div[@class='playbutton-cont col' and not(contains(@style,'none'))]";
