339117555f58e2d734ba48c052ff92e3aa257744
jakusotsu <psi_jedi@hotmail.com>
2016-12-02 00:48:17 -0800

    Change play and pause button to find via contains

--- data/netflix.com-view.js
+++ data/netflix.com-view.js
@@ -3,8 +3,10 @@
  * MediaKeys namespace.
  */
 if (typeof MediaKeys == "undefined") var MediaKeys = {};
-
-MediaKeys.playButton = "//div[@class='player-control-button player-play-pause play icon-player-play']";
-MediaKeys.pauseButton = "//div[@class='player-control-button player-play-pause pause icon-player-pause']";
+//
+//MediaKeys.playButton = "//div[@class='player-control-button player-play-pause play icon-player-play']";
+//MediaKeys.pauseButton = "//div[@class='player-control-button player-play-pause pause icon-player-pause']";
+MediaKeys.playButton = "//div[contains(@class,'player-play-pause')]";
+MediaKeys.pauseButton = "//div[contains(@class,'player-play-pause')]";
 MediaKeys.skipButton = "//div[@class='player-control-button player-next-episode container-icon-player-next-episode']";
 MediaKeys.previousButton = "//a[contains(@class,'player-back-to-browsing')]";
