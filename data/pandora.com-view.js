ea7f0fe5e59271174f282abc783ba3d13faf8f3d
Carlin <carlin.q.scott@gmail.com>
2017-02-09 22:02:42 -0800

    support for Pandora beta

--- data/pandora.com-view.js
+++ data/pandora.com-view.js
@@ -3,8 +3,9 @@
  */
 if (typeof MediaKeys == "undefined") var MediaKeys = {};
 
-MediaKeys.playButton = "//div[@class='playButton' and not(contains(@style,'none'))]";
-MediaKeys.pauseButton = "//div[@class='pauseButton' and not(contains(@style,'none'))]";
-MediaKeys.skipButton = "//div[@class='skipButton']";
-MediaKeys.trackInfoContainer = "//*[@id='trackInfoContainer']";
-MediaKeys.trackInfo = "//*[contains(@class,'trackData')]";
+MediaKeys.playButton = "//*[@data-qa='play_button' or @class='playButton' and not(contains(@style,'none'))]";
+MediaKeys.pauseButton = "//*[@data-qa='pause_button' or @class='pauseButton' and not(contains(@style,'none'))]";
+MediaKeys.previousButton = "//*[@data-qa='replay_button' or @class='skipButton']";
+MediaKeys.skipButton = "//*[@data-qa='skip_button']";
+MediaKeys.trackInfoContainer = "//*[contains(@data-reactid,'NowPlaying') or @id='trackInfoContainer']";
+MediaKeys.trackInfo = "//*[class='nowPlayingTopInfo__current' or contains(@class,'trackData')]";
