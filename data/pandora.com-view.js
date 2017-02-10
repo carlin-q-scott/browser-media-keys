9aca11a7208a17a175771eb5de8fcb3eead0ec7e
Carlin <carlin.q.scott@gmail.com>
2017-02-09 22:06:30 -0800

    missed skipButton legacy support for Pandora

--- data/pandora.com-view.js
+++ data/pandora.com-view.js
@@ -6,6 +6,6 @@ if (typeof MediaKeys == "undefined") var MediaKeys = {};
 MediaKeys.playButton = "//*[@data-qa='play_button' or @class='playButton' and not(contains(@style,'none'))]";
 MediaKeys.pauseButton = "//*[@data-qa='pause_button' or @class='pauseButton' and not(contains(@style,'none'))]";
 MediaKeys.previousButton = "//*[@data-qa='replay_button' or @class='skipButton']";
-MediaKeys.skipButton = "//*[@data-qa='skip_button']";
+MediaKeys.skipButton = "//*[@data-qa='skip_button' or @class='skipButton']";
 MediaKeys.trackInfoContainer = "//*[contains(@data-reactid,'NowPlaying') or @id='trackInfoContainer']";
 MediaKeys.trackInfo = "//*[class='nowPlayingTopInfo__current' or contains(@class,'trackData')]";
