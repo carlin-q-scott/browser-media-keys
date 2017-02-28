c8c9874fe6babb56d3e5c890180a733df0166040
CennoxX <cesar.bernard@gmx.de>
2017-02-28 12:12:31 +0100

    Update and rename play.spotify.com-view.js to open.spotify.com-view.js
    Update for Spotify HTML5 redesign support.

---
+++ data/open.spotify.com-view.js
@@ -0,0 +1,9 @@
+/**
+ * MediaKeys namespace.
+ */
+if (typeof MediaKeys == "undefined") var MediaKeys = {};
+
+MediaKeys.playButton = "//button[contains(@class,'spoticon-play-32') and contains(@class, 'control-button')]";
+MediaKeys.pauseButton = "//button[contains(@class,'spoticon-pause-32') and contains(@class, 'control-button')]";
+MediaKeys.skipButton = "//button[contains(@class,'spoticon-skip-forward-24') and contains(@class, 'control-button')]";
+MediaKeys.previousButton = "//button[contains(@class,'spoticon-skip-back-24') and contains(@class, 'control-button')]";
