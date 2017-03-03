b1858acada66440e393fca9095cacb96d9487dd7
CennoxX <cesar.bernard@gmx.de>
2017-03-03 11:16:57 +0100

    Update open.spotify.com-view.js
    use spoticon-skip-forward-* instead of spoticon-skip-forward-24

--- data/open.spotify.com-view.js
+++ data/open.spotify.com-view.js
@@ -3,7 +3,7 @@
  */
 if (typeof MediaKeys == "undefined") var MediaKeys = {};
 
-MediaKeys.playButton = "//button[contains(@class,'spoticon-play-32') and contains(@class, 'control-button')]";
-MediaKeys.pauseButton = "//button[contains(@class,'spoticon-pause-32') and contains(@class, 'control-button')]";
-MediaKeys.skipButton = "//button[contains(@class,'spoticon-skip-forward-24') and contains(@class, 'control-button')]";
-MediaKeys.previousButton = "//button[contains(@class,'spoticon-skip-back-24') and contains(@class, 'control-button')]";
+MediaKeys.playButton = "//button[contains(concat(' ', normalize-space(@class), ' '), ' spoticon-play-')]";
+MediaKeys.pauseButton = "//button[contains(concat(' ', normalize-space(@class), ' '), ' spoticon-pause-')]";
+MediaKeys.skipButton = "//button[contains(concat(' ', normalize-space(@class), ' '), ' spoticon-skip-forward-')]";
+MediaKeys.previousButton = "//button[contains(concat(' ', normalize-space(@class), ' '), ' spoticon-skip-back-')]";
