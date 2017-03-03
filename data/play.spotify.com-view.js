31ecc3d9dba3c0c622827056acde973b697f644f
CennoxX <cesar.bernard@gmx.de>
2017-03-03 10:33:02 +0100

    Create play.spotify.com-view.js
    play.spotify.com and player.spotify.com are still used

---
+++ data/play.spotify.com-view.js
@@ -0,0 +1,10 @@
+/**
+* MediaKeys namespace.
+*/
+if (typeof MediaKeys == "undefined") var MediaKeys = {};
+
+MediaKeys.basePlayer = "//iframe[@id='app-player']";
+MediaKeys.playButton = "//button[@id='play-pause' and not(contains(@class,'playing'))]";
+MediaKeys.pauseButton = "//button[@id='play-pause' and contains(@class,'playing')]";
+MediaKeys.skipButton = "//button[@id='next']";
+MediaKeys.previousButton = "//button[@id='previous']"; 
