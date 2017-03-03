1140a9ac2a06ff1907657871689cce8d404eb95c
CennoxX <cesar.bernard@gmx.de>
2017-03-03 10:48:15 +0100

    Update player.spotify.com-view.js

--- data/player.spotify.com-view.js
+++ data/player.spotify.com-view.js
@@ -1,10 +1,10 @@
 /**
-* MediaKeys namespace.
-*/
+ * MediaKeys namespace.
+ */
 if (typeof MediaKeys == "undefined") var MediaKeys = {};
 
 MediaKeys.basePlayer = "//iframe[@id='main']";
 MediaKeys.playButton = "//button[@id='play' and not(contains(@class,'playing'))]";
 MediaKeys.pauseButton = "//button[@id='play' and contains(@class,'playing')]";
 MediaKeys.skipButton = "//button[@id='next']";
-MediaKeys.previousButton = "//button[@id='previous']"; 
+MediaKeys.previousButton = "//button[@id='previous']";
