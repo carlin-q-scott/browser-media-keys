e5ef4c7d92d2325040debd6ffe71297d02b00f61
RblSb <RblSb@nm.ru>
2017-04-29 06:26:44 +0300

    Update vk.com-view.js

--- data/vk.com-view.js
+++ data/vk.com-view.js
@@ -3,6 +3,6 @@
  */
 if (typeof MediaKeys == "undefined") var MediaKeys = {};
 
-MediaKeys.playButton = MediaKeys.pauseButton = "//button[@class='top_audio_player_btn top_audio_player_play']";
+MediaKeys.playButton = MediaKeys.pauseButton = "//button[@class='top_audio_player_btn top_audio_player_play _top_audio_player_play']";
 MediaKeys.skipButton = "//button[@class='top_audio_player_btn top_audio_player_next']";
 MediaKeys.previousButton = "//button[@class='top_audio_player_btn top_audio_player_prev']";
