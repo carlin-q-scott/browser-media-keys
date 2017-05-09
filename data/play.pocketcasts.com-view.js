f0808ae811dfeb51b60fcb81e3208387ddef54d7
Elliot Iddon <elliot.iddon@digitalbarriers.com>
2017-05-09 12:50:24 +0100

    Add pocketcasts

---
+++ data/play.pocketcasts.com-view.js
@@ -0,0 +1,9 @@
+/**
+ * MediaKeys namespace.
+ */
+if (typeof MediaKeys == "undefined") var MediaKeys = {};
+
+MediaKeys.playButton = "//div[@class='play_button']";
+MediaKeys.pauseButton = "//div[@class='pause_button']";
+MediaKeys.skipButton = "//div[@class='skip_forward_button']";
+MediaKeys.previousButton = "//div[@class='skip_back_button']";
