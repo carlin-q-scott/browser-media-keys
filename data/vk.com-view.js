d6ea494c862351c2a9de1e5338222a0c56fb3ad7
regs01 <megaone@yandex.ru>
2018-05-19 20:36:44 +0300

    VK.com play button fix

--- data/vk.com-view.js
+++ data/vk.com-view.js
@@ -3,6 +3,6 @@
  */
 if (typeof MediaKeys == "undefined") var MediaKeys = {};
 
-MediaKeys.playButton = MediaKeys.pauseButton = "//button[contains(@class,'top_audio_player_btn') and contains(@class,'top_audio_player_play')]";
+MediaKeys.playButton = MediaKeys.pauseButton = "//button[contains(@class,'top_audio_player_btn') and contains(@class,'top_audio_player_play') and contains(@class,'_top_audio_player_play')]";
 MediaKeys.skipButton = "//button[contains(@class,'top_audio_player_btn') and contains(@class,'top_audio_player_next')]";
 MediaKeys.previousButton = "//button[contains(@class,'top_audio_player_btn') and contains(@class,'top_audio_player_prev')]";
